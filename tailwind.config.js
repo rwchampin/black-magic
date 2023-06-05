fa8200b62e45b03cd47db0a16917e7db18be04df
Ryan Champin <rwchampin@gmail.com>
2023-06-05 17:54:07 -0400

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    
+    borderRadius: {
+      none: '0',
+      sm: '0.125rem',
+      DEFAULT: '0.85rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      xl: '0.75rem',
+    },
+    extend: {
+      fontFamily: {
+        apercu: ['var(--font-apercu)'],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
